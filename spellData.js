2b00f6a40b14544f518206cdfa09559da960395e
David Corne <david.corne@autodesk.com>
2020-07-16 18:12:00 +0100

    Implement spell level and fix the test

--- spellData.js
+++ spellData.js
@@ -1,5 +1,25 @@
 const normalSpellLevel = function (monsterLevel) {
-
+  if (monsterLevel === -1) {
+    return 1
+  } else if (monsterLevel <= 1) {
+    return 2
+  } else if (monsterLevel <= 2) {
+    return 3
+  } else if (monsterLevel <= 3) {
+    return 4
+  } else if (monsterLevel <= 5) {
+    return 5
+  } else if (monsterLevel <= 7) {
+    return 6
+  } else if (monsterLevel <= 9) {
+    return 7
+  } else if (monsterLevel <= 11) {
+    return 8
+  } else if (monsterLevel <= 13) {
+    return 9
+  } else if (monsterLevel <= 15) {
+    return 10
+  }
 }
 
 const spellData = [
