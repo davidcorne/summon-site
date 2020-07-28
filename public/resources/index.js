57f48b89b9c557d81d3a8b434e825c020da0e3fa
David Corne <david.corne@autodesk.com>
2020-07-28 12:40:30 +0100

    Template the basic actions

--- public/resources/index.js
+++ public/resources/index.js
@@ -58,10 +58,97 @@ SummonSite.filterTable = function () {
 
 SummonSite.showMonster = function (monster) {
   const sidebar = document.getElementsByClassName('sidebar')[0]
-  sidebar.innerHTML = ''
-  const p = document.createElement('p')
-  p.innerHTML = monster.name
-  sidebar.appendChild(p)
+  sidebar.innerHTML = `
+  <h1 class="creature-name">
+    <a>${monster.name}</a>
+    <span>Creature ${monster.level}
+  </h1>
+  <ul class="traits">
+    ${monster.traits.map(trait =>
+      `<li>${trait}</li>`).join('')}
+  </ul>
+  <b>Source</b>
+    ${monster.source.map(source =>
+      `<i>${source.abbr} pg. ${source.page_start}</i>`).join('')}
+  <br>
+  <b>Senses</b>
+    ${monster.senses.map(sense =>
+      `${sense}`
+    )}
+  <br>
+  <b>Languages</b>
+    ${monster.languages ? monster.languages.map(language =>
+      `${language}`
+    ) : ''}
+  <br>
+  <b>Skills</b>
+    ${monster.skills.map(skill =>
+      `<u>${skill.name}</u>${skill.bonus}${skill.misc ? ' (' + skill.misc + ')' : ''}`
+    )}
+  <br>
+  <b>Str</b>
+  ${monster.ability_mods.str_mod}
+  <b>Dex</b>
+  ${monster.ability_mods.dex_mod}
+  <b>Con</b>
+  ${monster.ability_mods.con_mod}
+  <b>Int</b>
+  ${monster.ability_mods.int_mod}
+  <b>Wis</b>
+  ${monster.ability_mods.wis_mod}
+  <b>Cha</b>
+  ${monster.ability_mods.cha_mod}
+  <br>
+  <p>Sense Abilities</p>
+  <hr>
+  <b>AC</b>
+  ${monster.ac};
+  <b>Fort</b>
+  ${monster.saves.fort}
+  ${monster.saves.fort_misc ? '(' + monster.saves.fort_misc + ')' : ''}
+  ,
+  <b>Reflex</b>
+  ${monster.saves.ref}
+  ${monster.saves.ref_misc ? '(' + monster.saves.ref_misc + ')' : ''}
+  ,
+  <b>Will</b>
+  ${monster.saves.will}
+  ${monster.saves.will_misc ? '(' + monster.saves.will_misc + ')' : ''}
+  ${monster.saves.misc ? ';' + monster.saves.misc : ''}
+  <br>
+  <b>HP</b>
+  ${monster.hp}${monster.hp_misc ? ' (' + monster.hp_misc + ')' : ''};
+  ${monster.immunities ? `<b>Immunities</b> 
+    ${monster.immunities.map(immunity =>
+      `${immunity}`
+      )}
+    `
+    : ''}
+  ${monster.resistances ? `<b>Resistances</b> 
+    ${monster.resistances.map(resistance =>
+      `${resistance.type} ${resistance.amount}`
+      )}
+    `
+    : ''}
+  ${monster.weaknesses ? `<b>Weaknesses</b> 
+    ${monster.weaknesses.map(weakness =>
+      `${weakness.type} ${weakness.amount}`
+      )}
+    `
+    : ''}
+  <p>Automatic Abilities</p>
+  <hr>
+  <b>Speed</b>
+  ${monster.speed.map(speed =>
+    `${speed.type} ${speed.amount} feet`
+  )}
+  <br>
+  <p>Melee</p>
+  <p>Ranged</p>
+  <p>Spell List</p>
+  <p>Rituals</p>
+  <p>Proactive Abilities</p>
+  `
 }
 
 if ('serviceWorker' in navigator) {
